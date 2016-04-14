809f156b5c87e7a3a3aa5ac582fdcf559fb2ac8d
smiling2727 <xinyuel527@gmail.com>
2016-04-14 15:18:44 +0800

    Added files via upload

--- docatch.py
+++ docatch.py
@@ -8,12 +8,15 @@ from webcatch import catch
 
 file_path = '/Users/Smiling/Desktop/PythonCatch/test_catch.txt'
 catch.printTitle(file_path)
-for i in range (10000000,10005000):
-    #10001442么有
+for i in range (10005994,10006000):
     web_path = 'http://invest.ppdai.com/loan/info?id='+str(i)
-    soup = catch.connectInternet(web_path)
-    catch.catchContent(soup, file_path,i)
-    print 'OK'
+    try:
+        soup = catch.connectInternet(web_path)
+        catch.catchContent(soup, file_path,i)
+        print 'OK'
+    except:
+        print 'Content Error'
+
 
 
 
